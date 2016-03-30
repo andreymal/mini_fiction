f63255f424401b84c5f7ee50e669feba70b53057
andreymal <andriyano-31@mail.ru>
2016-03-30 15:59:35 +0300

    Celery Sphinx tasks retrying if connection refused

--- mini_fiction/tasks.py
+++ mini_fiction/tasks.py
@@ -1,6 +1,8 @@
 #!/usr/bin/env python
 # -*- coding: utf-8 -*-
 
+from functools import wraps
+
 from pony.orm import db_session
 
 from mini_fiction.models import Story, Chapter
@@ -9,9 +11,23 @@ from mini_fiction.models import Story, Chapter
 tasks = {}
 
 
-def task(f, name=None, **kwargs):
-    tasks[name or f.__name__] = (f, kwargs)
-    return f
+def task(name=None, **kwargs):
+    def decorator(f):
+        tasks[name or f.__name__] = (f, kwargs)
+        return f
+    return decorator
+
+
+def task_sphinx_retrying(f):
+    @task(bind=True)
+    @wraps(f)
+    def decorated(self, *args, **kwargs):
+        from MySQLdb import OperationalError
+        try:
+            return f(*args, **kwargs)
+        except OperationalError as exc:
+            self.retry(countdown=30, max_retries=30, exc=exc)
+    return decorated
 
 
 def apply_for_app(app):
@@ -20,14 +36,14 @@ def apply_for_app(app):
         app.tasks[name] = app.celery.task(**kwargs)(f)
 
 
-@task
+@task()
 @db_session
 def sendmail(to, subject, body, fro=None, config=None):
     from mini_fiction.utils import misc
     misc.sendmail(to, subject, body, fro=fro, config=config)
 
 
-@task
+@task_sphinx_retrying
 @db_session
 def sphinx_update_story(story_id, update_fields):
     story = Story.get(id=story_id)
@@ -37,7 +53,7 @@ def sphinx_update_story(story_id, update_fields):
     story.bl.search_update(update_fields)
 
 
-@task
+@task_sphinx_retrying
 @db_session
 def sphinx_update_chapter(chapter_id):
     chapter = Chapter.get(id=chapter_id)
@@ -48,7 +64,7 @@ def sphinx_update_chapter(chapter_id):
     chapter.story.bl.search_update(('words',))
 
 
-@task
+@task_sphinx_retrying
 @db_session
 def sphinx_update_comments_count(story_id):
     story = Story.get(id=story_id)
@@ -57,12 +73,12 @@ def sphinx_update_comments_count(story_id):
     story.bl.search_update(('comments',))
 
 
-@task
+@task_sphinx_retrying
 def sphinx_delete_story(story_id):
     Story.bl.delete_stories_from_search((story_id,))
 
 
-@task
+@task_sphinx_retrying
 @db_session
 def sphinx_delete_chapter(story_id, chapter_id):
     Chapter.bl.delete_chapters_from_search((chapter_id,))

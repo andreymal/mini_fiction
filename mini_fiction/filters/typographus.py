2cb72dc3d97f07d93f1c4233b1eaf9645e08b0dc
andreymal <andriyano-31@mail.ru>
2018-03-02 19:51:14 +0300

    Small fix for typographus

--- mini_fiction/filters/typographus.py
+++ mini_fiction/filters/typographus.py
@@ -6,14 +6,14 @@ from .base import html_text_transform
 
 
 typo_patterns = [
-    (r'(^|\n)[-—]-?[ \t\r\f\v]+', '\\1— '),
+    (r'(^|\n)(<[^>]+?>)?[-—]-?[ \t\r\f\v]+', '\\1\\2— '),
     (r'[ \t\r\f\v]+[-—]-?[ \t\r\f\v]+', '\xa0— '),
 ]
-typo_patterns = [(re.compile(p), r) for p, r in typo_patterns]
+typo_patterns = [(re.compile(p_), r_) for p_, r_ in typo_patterns]
 
 
 @html_text_transform
 def typo(text):
     for p, r in typo_patterns:
-        text = p.subn(r, text)[0]  # pylint: disable=no-member
+        text = p.subn(r, text)[0]
     return text

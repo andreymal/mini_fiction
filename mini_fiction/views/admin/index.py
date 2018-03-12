02324e94862fc8bb18a05a85cb582cdd3740a361
andreymal <andriyano-31@mail.ru>
2018-03-12 20:03:14 +0300

    Fancy admin index page

--- mini_fiction/views/admin/index.py
+++ mini_fiction/views/admin/index.py
@@ -1,11 +1,13 @@
 #!/usr/bin/env python3
 # -*- coding: utf-8 -*-
 
-from flask import Blueprint, render_template, url_for
+from datetime import datetime, timedelta
+
+from flask import Blueprint, render_template, url_for, current_app
 from flask_babel import gettext
 from pony.orm import db_session
 
-from mini_fiction.models import AdminLog
+from mini_fiction import models
 from mini_fiction.utils.views import admin_required
 
 bp = Blueprint('admin_index', __name__)
@@ -15,11 +17,36 @@ bp = Blueprint('admin_index', __name__)
 @db_session
 @admin_required
 def index():
-    log = AdminLog.bl.get_list()
+    log = models.AdminLog.bl.get_list()
     for x in log['items']:
         x['admin_url'] = get_adminlog_object_url(x['type_str'], x['object_id'])
 
-    return render_template('admin/index.html', page_title=gettext('Administration'), log=log)
+    ctx = {
+        'page_title': gettext('Administration'),
+        'log': log,
+    }
+
+    last_24_hours = datetime.utcnow() - timedelta(days=1)
+
+    # Статистика по жалобам
+    ctx['abuse_unresolved_count'] = models.AbuseReport.select(lambda x: not x.ignored and x.resolved_at is None).count()
+    abuse_last = models.AbuseReport.select().order_by(models.AbuseReport.created_at.desc()).first()
+    ctx['abuse_last'] = abuse_last.created_at if abuse_last else None
+
+    # Статистика по оценкам
+    ctx['vote_last_count'] = models.Vote.select(lambda x: x.revoked_at is None and x.updated >= last_24_hours).count()
+    ctx['vote_last'] = models.Vote.select().order_by(models.Vote.updated.desc()).first()
+
+    # Статистика по пользователям
+    ctx['author_last_count'] = models.Author.select(lambda x: x.activated_at >= last_24_hours).count()
+    ctx['author_last'] = models.Author.select().order_by(models.Author.id.desc()).first()
+
+    # Статистика по регистрациям
+    account_registration_mindate = datetime.utcnow() - timedelta(days=current_app.config['ACCOUNT_ACTIVATION_DAYS'])
+    ctx['registrationprofile_count'] = models.RegistrationProfile.select(lambda x: x.created_at >= account_registration_mindate).count()
+    ctx['registrationprofile_last'] = models.RegistrationProfile.select().order_by(models.RegistrationProfile.id.desc()).first()
+
+    return render_template('admin/index.html', **ctx)
 
 
 def get_adminlog_object_url(typ, pk):

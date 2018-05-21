ed6d1d36c3a471fadd551eef078aac39b2957bc9
andreymal <andriyano-31@mail.ru>
2018-05-21 21:11:56 +0300

    Fixed load_app in cli

--- mini_fiction/management/main.py
+++ mini_fiction/management/main.py
@@ -3,13 +3,19 @@
 
 
 def run():
-    from flask.cli import ScriptInfo
+    from flask.helpers import get_load_dotenv
+    from flask.cli import ScriptInfo, load_dotenv
 
     from mini_fiction.management import manager
 
     manager.populate_commands()
+
+    # Необходимо создать приложение заранее для загрузки команд из плагинов
+    if get_load_dotenv(manager.cli.load_dotenv):
+        load_dotenv()
     obj = ScriptInfo(create_app=manager.cli.create_app)
-    obj.load_app()  # Необходимо для загрузки команд из плагинов
+    obj.load_app()
+
     return manager.cli(obj=obj)
 
 

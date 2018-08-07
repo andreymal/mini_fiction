c00b4a6eab6a76ede5802c802e7898a45a2d1678
andreymal <andriyano-31@mail.ru>
2018-08-08 02:35:54 +0300

    Tags: add autocomplete API

--- mini_fiction/views/tags.py
+++ mini_fiction/views/tags.py
@@ -1,7 +1,9 @@
 #!/usr/bin/env python3
 # -*- coding: utf-8 -*-
 
-from flask import Blueprint, current_app, render_template, abort, redirect, url_for
+import json
+
+from flask import Blueprint, current_app, request, render_template, abort, redirect, url_for
 from flask_login import current_user
 from pony.orm import db_session
 
@@ -48,3 +50,68 @@ def tag_index(tag_name, page):
         page_obj=page_obj,
         **cached_lists([x.id for x in objects])
     )
+
+
+@bp.route('/tags/autocomplete.json')
+@db_session
+def autocomplete():
+    tag_name = (request.args.get('tag') or '').strip()
+
+    tags = []
+    is_default = False
+    data = None
+
+    if len(tag_name) < 2:
+        # Предложения по умолчанию, чтобы пусто не было
+        # (кэшируются)
+        is_default = True
+        data = current_app.cache.get('tags_autocomplete_default')
+        if not data:
+            tags = _get_default_tags()
+
+    else:
+        tags = Tag.bl.search_by_prefix(tag_name)
+
+    if not data:
+        aliases = Tag.bl.get_aliases_for(tags)
+
+        result = {
+            'success': True,
+            'tags': [{
+                'id': tag.id,
+                'name': tag.name,
+                'url': url_for('tags.tag_index', tag_name=tag.iname),
+                'description': tag.description,
+                'stories_count': tag.published_stories_count,
+                'aliases': [x.iname for x in aliases[tag.id]],
+                'color': tag.get_color(),
+            } for tag in tags],
+        }
+        data = json.dumps(result, ensure_ascii=False, sort_keys=True)
+        if is_default:
+            current_app.cache.set('tags_autocomplete_default', data, timeout=600)
+
+    response = current_app.response_class(data, mimetype='application/json')
+    response.headers['X-Robots-Tag'] = 'noindex'
+    return response
+
+
+def _get_default_tags(limit=20):
+    tags = []
+    all_tags = Tag.bl.get_all_tags()
+
+    # В начале основные теги
+    tags = [x for x in all_tags if x.is_main_tag]
+    tags.sort(key=lambda x: (x.category.id if x.category else 2 ** 31, x.iname))
+
+    # Потом двадцать самых популярных
+    cnt = 0
+    all_tags.sort(key=lambda x: x.published_stories_count, reverse=True)
+    for x in all_tags:
+        if not x.is_main_tag:
+            tags.append(x)
+            cnt += 1
+            if cnt >= limit:
+                break
+
+    return tags

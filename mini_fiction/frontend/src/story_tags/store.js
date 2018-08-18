83ac48404b6d527d7dd440391846d52b92676c12
Andriy Kushnir (Orhideous) <me@orhideous.name>
2018-08-18 13:44:37 +0300

    Revert "Add support for ephemeral values in store"
    This reverts commit 009a861

--- mini_fiction/frontend/src/story_tags/store.js
+++ mini_fiction/frontend/src/story_tags/store.js
@@ -1,16 +1,8 @@
 let isSettled = false;
 let resolver;
-
-const ephemeralStore = [];
 const store = new Promise((res) => { resolver = res; });
 
-const addEphemeral = value => ephemeralStore.push(value);
-
-const getEphemeral = () => Promise.all(ephemeralStore);
-
-const getStore = () => Promise
-  .all([store, getEphemeral()])
-  .then(([data, ephemeral]) => data.concat(ephemeral));
+const getStore = () => store;
 
 const setStoreFromUrl = (url) => {
   if (isSettled) return;
@@ -24,7 +16,6 @@ const setStoreFromUrl = (url) => {
 };
 
 export {
-  addEphemeral,
   getStore,
   setStoreFromUrl,
 };

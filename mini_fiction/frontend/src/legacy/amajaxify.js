778ef002ec0d2c653e2621bd7e5e2216cc94b69a
Andriy Kushnir (Orhideous) <me@orhideous.name>
2018-08-25 23:50:19 +0300

    Use plain CommonJS export in sake of simplicity

--- mini_fiction/frontend/src/legacy/amajaxify.js
+++ mini_fiction/frontend/src/legacy/amajaxify.js
@@ -987,14 +987,4 @@ var amajaxify = {
     }
 };
 
-(function (root, factory) {
-  if (typeof define === 'function' && define.amd) {
-    define([], factory);
-  } else if (typeof exports === 'object') {
-    module.exports = factory();
-  } else {
-    root.amajaxify = factory();
-  }
-}(this, function () {
-  return amajaxify;
-}));
+export default amajaxify;

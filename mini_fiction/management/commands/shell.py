507b0572b5a18974d9f350756ec6b85eba557ebd
andreymal <andriyano-31@mail.ru>
2019-02-28 01:33:53 +0300

    Improve context of the interactive shell

--- mini_fiction/management/commands/shell.py
+++ mini_fiction/management/commands/shell.py
@@ -1,7 +1,7 @@
 #!/usr/bin/env python3
 # -*- coding: utf-8 -*-
 
-from flask import current_app
+from flask import current_app, g
 from pony.orm import db_session
 
 from mini_fiction.management.manager import cli
@@ -12,9 +12,14 @@ def shell():
     import code
     import mini_fiction
     from mini_fiction import models
+
+    g.locale = current_app.extensions['babel'].default_locale
+
     with db_session:
         code.interact(local={
             'mini_fiction': mini_fiction,
             'app': current_app._get_current_object(),
+            'g': g,
             'm': models,
+            'system_user': models.Author.get(id=current_app.config['SYSTEM_USER_ID']),
         })

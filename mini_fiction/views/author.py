25cc1a547bd790cd91c26a2ceef3c42b0321b849
andreymal <andriyano-31@mail.ru>
2020-10-03 21:33:22 +0300

    Split settings into separate pages

--- mini_fiction/views/author.py
+++ mini_fiction/views/author.py
@@ -16,6 +16,34 @@ from mini_fiction.validation import ValidationError
 bp = Blueprint('author', __name__)
 
 
+def _get_user_for_edit(user_id=None, select_for_update=False):
+    if user_id is None and current_user.id is not None:
+        user_id = current_user.id
+    if user_id is None:
+        abort(404)
+
+    try:
+        user_id = int(user_id)
+    except ValueError:
+        abort(404)
+
+    if user_id != current_user.id and not current_user.is_superuser:
+        abort(403)
+
+    try:
+        if select_for_update:
+            user = Author.get_for_update(id=user_id)
+        else:
+            user = Author.get(id=user_id)
+    except ValueError:  # int out of range
+        user = None
+
+    if user is None:
+        abort(404)
+    return user
+
+
+
 @bp.route('/profile/', defaults={'user_id': None, 'comments_page': 1})
 @bp.route('/profile/comments/page/<int:comments_page>/', defaults={'user_id': None})
 @bp.route('/<user_id>/', defaults={'comments_page': 1})
@@ -93,185 +121,250 @@ def info(user_id=None, comments_page=1):
     return render_template(template, **data)
 
 
-@bp.route('/profile/edit/', methods=('GET', 'POST'))
+@bp.route('/profile/edit/', defaults={'user_id': None}, methods=('GET', 'POST'))
+@bp.route('/<user_id>/edit/', methods=('GET', 'POST'))
 @db_session
 @login_required
-def edit():
-    author = current_user._get_current_object()
+def edit_general(user_id=None):
+    user = _get_user_for_edit(user_id, select_for_update=request.method == 'POST')
 
-    cep = ChangeEmailProfile.get(user=author)
-    new_email = cep.email if cep else None
-
-    data = {'page_title': gettext('Profile settings'), 'user': author}
-
-    has_avatar = bool(author.avatar_small or author.avatar_medium or author.avatar_large)
+    has_avatar = bool(user.avatar_small or user.avatar_medium or user.avatar_large)
     avatar_uploading = bool(current_app.config['AVATARS_UPLOADING'])
 
-    profile_form = None
-    email_form = None
-    password_form = None
-    prefs_form = None
-    subs_form = None
+    contacts = list(Contact.select(lambda x: x.author == user).order_by(Contact.id))
 
-    password_form_errors = []
-    email_form_errors = []
-
-    if request.method == 'POST' and request.form:
-        if 'save_password' in request.form:
-            password_form = AuthorEditPasswordForm()
-            if password_form.validate_on_submit():
-                if not author.bl.check_password(password_form.old_password.data):
-                    password_form_errors.append(gettext('Old password is incorrect'))
-                elif not author.bl.is_password_good(password_form.new_password_1.data):
-                    password_form_errors.append(gettext('Password is too bad, please change it'))
-                else:
-                    author.bl.set_password(password_form.new_password_1.data)
-                    logout_user()
-                    author.bl.reset_session_token()
-                    return redirect(url_for('auth.login'))
-
-        if 'save_profile' in request.form:
-            profile_form = AuthorEditProfileForm()
-            if not has_avatar:
-                del profile_form.delete_avatar
-            if not avatar_uploading:
-                del profile_form.avatar
-
-            if profile_form.validate_on_submit():
-                try:
-                    author.bl.update(profile_form.data)
-                except ValidationError as exc:
-                    profile_form.set_errors(exc.errors)
-                else:
-                    data['profile_ok'] = True
-
-        if 'save_email' in request.form:
-            email_form = AuthorEditEmailForm()
-            if email_form.validate_on_submit():
-                if not author.bl.check_password(email_form.password.data):
-                    email_form_errors.append(gettext('Password is incorrect'))
-                else:
-                    try:
-                        data['email_changed'] = author.bl.update_email_with_confirmation(email_form.email.data)
-                        data['email_ok'] = True
-                    except ValidationError as exc:
-                        email_form.set_errors(exc.errors)
-
-        if 'save_prefs' in request.form:
-            prefs_form = AuthorEditPrefsForm()
-            if prefs_form.validate_on_submit():
-                prefs_data = dict(prefs_form.data)
-                if prefs_data.get('header_mode') == 0:
-                    prefs_data['header_mode'] = 'off'
-                elif prefs_data.get('header_mode') == 1:
-                    prefs_data['header_mode'] = 'l'
-                elif prefs_data.get('header_mode') == 2:
-                    prefs_data['header_mode'] = 'ls'
-                else:
-                    prefs_data.pop('header_mode', None)
-                author.bl.update(prefs_data)
-                data['prefs_ok'] = True
-
-        if 'save_subs' in request.form:
-            subs_form = AuthorEditSubscriptionsForm()
-            if subs_form.validate_on_submit():
-                author.bl.update_email_subscriptions({
-                    'abuse_report': subs_form.email_abuse_report.data,
-                    'story_pubrequest': subs_form.email_story_pubrequest.data,
-                    'story_publish': subs_form.email_story_publish.data,
-                    'story_publish_noappr': subs_form.email_story_publish_noappr.data,
-                    'story_draft': subs_form.email_story_draft.data,
-                    'story_delete': subs_form.email_story_delete.data,
-                    'story_reply': subs_form.email_story_reply.data,
-                    'story_lreply': subs_form.email_story_lreply.data,
-                    'news_reply': subs_form.email_news_reply.data,
-                })
-                author.bl.update_tracker_subscriptions({
-                    'story_publish': subs_form.tracker_story_publish.data,
-                    'story_draft': subs_form.tracker_story_draft.data,
-                    'story_reply': subs_form.tracker_story_reply.data,
-                    'story_lreply': subs_form.tracker_story_lreply.data,
-                    'news_reply': subs_form.tracker_news_reply.data,
-                })
-                data['subs_ok'] = True
-
-    if not profile_form:
-        contacts = list(Contact.select(lambda x: x.author == author).order_by(Contact.id))
-        profile_form = AuthorEditProfileForm(formdata=None, data={
-            'bio': author.bio,
-            'contacts': [
-                {'name': c.name, 'value': c.value}
-                for c in contacts
-            ] + [{'name': '', 'value': ''}]
-        })
-        if not has_avatar:
-            del profile_form.delete_avatar
-        if not avatar_uploading:
-            del profile_form.avatar
-    if not email_form:
-        email_form = AuthorEditEmailForm(formdata=None, data={'email': new_email or author.email})
-    if not password_form:
-        password_form = AuthorEditPasswordForm(formdata=None)
-    if not prefs_form:
-        prefs_form = AuthorEditPrefsForm(formdata=None, data={
-                # 'excluded_categories': author.excluded_categories_list,
-                'detail_view': author.detail_view,
-                'nsfw': author.nsfw,
-                'comments_per_page': (
-                    author.comments_per_page
-                    if author.comments_per_page is not None
-                    else current_app.config['COMMENTS_COUNT']['page']
-                ),
-                'comments_maxdepth': (
-                    author.comments_maxdepth
-                    if author.comments_maxdepth is not None
-                    else current_app.config['COMMENTS_TREE_MAXDEPTH']
-                ),
-                'comment_spoiler_threshold': (
-                    author.comment_spoiler_threshold
-                    if author.comment_spoiler_threshold is not None
-                    else current_app.config['COMMENT_SPOILER_THRESHOLD']
-                ),
-                'header_mode': {'': 2, 'off': 0, 'l': 1, 'ls': 2}[author.header_mode],
-            }
-        )
-    if not subs_form:
-        silent_email = author.silent_email_list
-        silent_tracker = author.silent_tracker_list
-        subs_form = AuthorEditSubscriptionsForm(formdata=None, data={
-            'email_abuse_report': 'abuse_report' not in silent_email,
-            'email_story_pubrequest': 'story_pubrequest' not in silent_email,
-            'email_story_publish_noappr': 'story_publish_noappr' not in silent_email,
-            'email_story_delete': 'story_delete' not in silent_email,
-
-            'email_story_publish': 'story_publish' not in silent_email,
-            'tracker_story_publish': 'story_publish' not in silent_tracker,
-
-            'email_story_draft': 'story_draft' not in silent_email,
-            'tracker_story_draft': 'story_draft' not in silent_tracker,
-
-            'email_story_reply': 'story_reply' not in silent_email,
-            'tracker_story_reply': 'story_reply' not in silent_tracker,
-
-            'email_story_lreply': 'story_lreply' not in silent_email,
-            'tracker_story_lreply': 'story_lreply' not in silent_tracker,
-
-            'email_news_reply': 'news_reply' not in silent_email,
-            'tracker_news_reply': 'news_reply' not in silent_tracker,
-        })
-    data.update({
-        'profile_form': profile_form,
-        'email_form': email_form,
-        'new_email': new_email,
-        'password_form': password_form,
-        'prefs_form': prefs_form,
-        'subs_form': subs_form,
-        'profile_form_errors': [],
-        'email_form_errors': email_form_errors,
-        'password_form_errors': password_form_errors,
-        'prefs_form_errors': [],
+    form = AuthorEditProfileForm(data={
+        'bio': user.bio,
+        'contacts': [
+            {'name': c.name, 'value': c.value}
+            for c in contacts
+        ] + [{'name': '', 'value': ''}]
     })
-    return render_template('author_profile_edit.html', **data)
+
+    if not has_avatar:
+        del form.delete_avatar
+    if not avatar_uploading:
+        del form.avatar
+
+    ctx = {
+        'page_title': 'Основные настройки',
+        'settings_tab': 'general',
+        'user': user,
+        'user_is_current': user.id == current_user.id,
+        'form': form,
+        'non_field_errors': [],
+        'saved': False,
+    }
+
+    if request.method == 'POST':
+        if form.validate_on_submit():
+            try:
+                user.bl.update(form.data)
+            except ValidationError as exc:
+                form.set_errors(exc.errors)
+            else:
+                ctx['saved'] = True
+
+    return render_template('profile_edit_general.html', **ctx)
+
+
+@bp.route('/profile/edit/mail/', defaults={'user_id': None}, methods=('GET', 'POST'))
+@bp.route('/<user_id>/edit/mail/', methods=('GET', 'POST'))
+@db_session
+@login_required
+def edit_mail(user_id=None):
+    user = _get_user_for_edit(user_id, select_for_update=request.method == 'POST')
+
+    cep = ChangeEmailProfile.get(user=user)
+    new_email = cep.email if cep else None
+
+    form = AuthorEditEmailForm(data={'email': new_email or user.email})
+    non_field_errors = []
+
+    ctx = {
+        'page_title': 'Настройки почты',
+        'settings_tab': 'mail',
+        'user': user,
+        'user_is_current': user.id == current_user.id,
+        'form': form,
+        'non_field_errors': non_field_errors,
+        'new_email': new_email,
+        'email_changed': False,
+        'saved': False,
+    }
+
+    if request.method == 'POST':
+        if form.validate_on_submit():
+            if not user.bl.check_password(form.password.data):
+                non_field_errors.append(gettext('Password is incorrect'))
+            else:
+                try:
+                    ctx['email_changed'] = user.bl.update_email_with_confirmation(form.email.data)
+                    ctx['new_email'] = form.email.data if ctx['email_changed'] else None
+                    ctx['saved'] = True
+                except ValidationError as exc:
+                    form.set_errors(exc.errors)
+
+    return render_template('profile_edit_mail.html', **ctx)
+
+
+@bp.route('/profile/edit/security/', defaults={'user_id': None}, methods=('GET', 'POST'))
+@bp.route('/<user_id>/edit/security/', methods=('GET', 'POST'))
+@db_session
+@login_required
+def edit_security(user_id=None):
+    user = _get_user_for_edit(user_id, select_for_update=request.method == 'POST')
+
+    password_form = AuthorEditPasswordForm()
+    password_form_errors = []
+
+    ctx = {
+        'page_title': 'Настройки безопасности',
+        'settings_tab': 'security',
+        'user': user,
+        'user_is_current': user.id == current_user.id,
+        'password_form': password_form,
+        'password_form_errors': password_form_errors,
+    }
+
+    if request.method == 'POST':
+        if password_form.validate_on_submit():
+            if not user.bl.check_password(password_form.old_password.data):
+                password_form_errors.append(gettext('Old password is incorrect'))
+            elif not user.bl.is_password_good(password_form.new_password_1.data):
+                password_form_errors.append(gettext('Password is too bad, please change it'))
+            else:
+                user.bl.set_password(password_form.new_password_1.data)
+                logout_user()
+                user.bl.reset_session_token()
+                return redirect(url_for('auth.login'))
+
+    return render_template('profile_edit_security.html', **ctx)
+
+
+@bp.route('/profile/edit/personal/', defaults={'user_id': None}, methods=('GET', 'POST'))
+@bp.route('/<user_id>/edit/personal/', methods=('GET', 'POST'))
+@db_session
+@login_required
+def edit_personal(user_id=None):
+    user = _get_user_for_edit(user_id, select_for_update=request.method == 'POST')
+
+
+    form = AuthorEditPrefsForm(data={
+        # 'excluded_categories': user.excluded_categories_list,
+        'detail_view': user.detail_view,
+        'nsfw': user.nsfw,
+        'comments_per_page': (
+            user.comments_per_page
+            if user.comments_per_page is not None
+            else current_app.config['COMMENTS_COUNT']['page']
+        ),
+        'comments_maxdepth': (
+            user.comments_maxdepth
+            if user.comments_maxdepth is not None
+            else current_app.config['COMMENTS_TREE_MAXDEPTH']
+        ),
+        'comment_spoiler_threshold': (
+            user.comment_spoiler_threshold
+            if user.comment_spoiler_threshold is not None
+            else current_app.config['COMMENT_SPOILER_THRESHOLD']
+        ),
+        'header_mode': {'': 2, 'off': 0, 'l': 1, 'ls': 2}[user.header_mode],
+    })
+    non_field_errors = []
+
+    ctx = {
+        'page_title': 'Настройки внешнего вида и отображения',
+        'settings_tab': 'personal',
+        'user': user,
+        'user_is_current': user.id == current_user.id,
+        'form': form,
+        'non_field_errors': non_field_errors,
+        'saved': False,
+    }
+
+    if request.method == 'POST':
+        if form.validate_on_submit():
+            prefs_data = dict(form.data)
+            if prefs_data.get('header_mode') == 0:
+                prefs_data['header_mode'] = 'off'
+            elif prefs_data.get('header_mode') == 1:
+                prefs_data['header_mode'] = 'l'
+            elif prefs_data.get('header_mode') == 2:
+                prefs_data['header_mode'] = 'ls'
+            else:
+                prefs_data.pop('header_mode', None)
+            user.bl.update(prefs_data)
+            ctx['saved'] = True
+
+    return render_template('profile_edit_personal.html', **ctx)
+
+
+@bp.route('/profile/edit/subscriptions/', defaults={'user_id': None}, methods=('GET', 'POST'))
+@bp.route('/<user_id>/edit/subscriptions/', methods=('GET', 'POST'))
+@db_session
+@login_required
+def edit_subscriptions(user_id=None):
+    user = _get_user_for_edit(user_id, select_for_update=request.method == 'POST')
+
+    silent_email = user.silent_email_list
+    silent_tracker = user.silent_tracker_list
+    form = AuthorEditSubscriptionsForm(data={
+        'email_abuse_report': 'abuse_report' not in silent_email,
+        'email_story_pubrequest': 'story_pubrequest' not in silent_email,
+        'email_story_publish_noappr': 'story_publish_noappr' not in silent_email,
+        'email_story_delete': 'story_delete' not in silent_email,
+
+        'email_story_publish': 'story_publish' not in silent_email,
+        'tracker_story_publish': 'story_publish' not in silent_tracker,
+
+        'email_story_draft': 'story_draft' not in silent_email,
+        'tracker_story_draft': 'story_draft' not in silent_tracker,
+
+        'email_story_reply': 'story_reply' not in silent_email,
+        'tracker_story_reply': 'story_reply' not in silent_tracker,
+
+        'email_story_lreply': 'story_lreply' not in silent_email,
+        'tracker_story_lreply': 'story_lreply' not in silent_tracker,
+
+        'email_news_reply': 'news_reply' not in silent_email,
+        'tracker_news_reply': 'news_reply' not in silent_tracker,
+    })
+    non_field_errors = []
+
+    ctx = {
+        'page_title': 'Настройки уведомлений и подписок',
+        'settings_tab': 'subscriptions',
+        'user': user,
+        'user_is_current': user.id == current_user.id,
+        'form': form,
+        'non_field_errors': non_field_errors,
+        'saved': False,
+    }
+
+    if request.method == 'POST':
+        if form.validate_on_submit():
+            user.bl.update_email_subscriptions({
+                'abuse_report': form.email_abuse_report.data,
+                'story_pubrequest': form.email_story_pubrequest.data,
+                'story_publish': form.email_story_publish.data,
+                'story_publish_noappr': form.email_story_publish_noappr.data,
+                'story_draft': form.email_story_draft.data,
+                'story_delete': form.email_story_delete.data,
+                'story_reply': form.email_story_reply.data,
+                'story_lreply': form.email_story_lreply.data,
+                'news_reply': form.email_news_reply.data,
+            })
+            user.bl.update_tracker_subscriptions({
+                'story_publish': form.tracker_story_publish.data,
+                'story_draft': form.tracker_story_draft.data,
+                'story_reply': form.tracker_story_reply.data,
+                'story_lreply': form.tracker_story_lreply.data,
+                'news_reply': form.tracker_news_reply.data,
+            })
+            ctx['saved'] = True
+
+    return render_template('profile_edit_subscriptions.html', **ctx)
 
 
 @bp.route('/<user_id>/ban/', methods=('POST',))

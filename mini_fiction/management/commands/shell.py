32fb3dd24e1371a77a1295fc120be39f4c84dba3
andreymal <andriyano-31@mail.ru>
2020-12-11 22:54:05 +0300

    Improve shell: add --command option, don't start interactive console for non-tty stdin

--- mini_fiction/management/commands/shell.py
+++ mini_fiction/management/commands/shell.py
@@ -1,6 +1,12 @@
 #!/usr/bin/env python3
 # -*- coding: utf-8 -*-
 
+# pylint: disable=exec-used
+
+import sys
+import select
+
+import click
 from flask import current_app, g
 from pony.orm import db_session
 
@@ -8,18 +14,30 @@ from mini_fiction.management.manager import cli
 
 
 @cli.command(help='Runs a Python interactive interpreter.')
-def shell():
+@click.option('-c', '--command', 'command', help='Instead of opening an interactive shell, run a command and exit.', required=False)
+def shell(command):
     import code
     import mini_fiction
     from mini_fiction import models
 
-    g.locale = current_app.extensions['babel'].default_locale
-
     with db_session:
-        code.interact(local={
+        g.locale = current_app.extensions['babel'].default_locale
+
+        local = {
             'mini_fiction': mini_fiction,
             'app': current_app._get_current_object(),
             'g': g,
             'm': models,
             'system_user': models.Author.get(id=current_app.config['SYSTEM_USER_ID']),
-        })
+        }
+
+        if command is not None:
+            exec(command, local)
+            return
+
+        # If stdin is not a tty, just execute the code without opening an interactive shell
+        if sys.platform != 'win32' and not sys.stdin.isatty() and select.select([sys.stdin.fileno()], [], [], 0)[0]:
+            exec(sys.stdin.read(), local)
+            return
+
+        code.interact(local=local)

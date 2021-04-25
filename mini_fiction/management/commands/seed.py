f054034da2666503217cdc21e0e89ddd5b94806d
andreymal <andriyano-31@mail.ru>
2021-04-25 21:43:01 +0300

    Improve seed options

--- mini_fiction/management/commands/seed.py
+++ mini_fiction/management/commands/seed.py
@@ -8,8 +8,9 @@ from mini_fiction.management.manager import cli
 
 
 @cli.command(help='Loads default fixtures into the database.')
-@click.option('-s', '--silent', 'silent', help='Don\'t print progress bar to console.', is_flag=True)
-def seed(silent):
+@click.option('-O', '--overwrite', 'overwrite', help='Overwrite existing objects', is_flag=True)
+@click.option("-v", "--verbose", "verbosity", count=True, help='Verbosity: -v prints status, -vv prints progress bar.')
+def seed(verbosity=0, overwrite=False):
     from mini_fiction import fixtures
     orm.sql_debug(False)
-    fixtures.seed(verbosity=1 if silent else 2, only_create=True)
+    fixtures.seed(verbosity=verbosity, only_create=not overwrite)

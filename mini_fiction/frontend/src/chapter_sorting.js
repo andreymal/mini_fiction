ccbe8a9d667983dedcf1d24fdf5dc18848f8f86b
Andriy Kushnir (Orhideous) <me@orhideous.name>
2021-05-20 06:58:30 +0300

    Drop core requirement from chapter_sorting chunk

--- mini_fiction/frontend/src/chapter_sorting.js
+++ mini_fiction/frontend/src/chapter_sorting.js
@@ -1,24 +1,30 @@
 import jQueryUI from './legacy/lib/jquery-ui-custom.min';
 import jQueryUITouchPunch from './legacy/lib/jquery-ui-touch-punch.min';
-import core from './legacy/core';
 import { postJSON } from './utils/ajax';
+import { notify, notifyError } from './utils/notifications';
 
 const sortEventFactory = (jQuery) => () => {
-  const items = jQuery('#sortable_chapters').sortable('toArray', { attribute: 'data-chapter' });
+  const items = jQuery('#sortable_chapters')
+    .sortable('toArray', { attribute: 'data-chapter' });
   const data = { chapters: [] };
   // eslint-disable-next-line no-plusplus
   for (let i = 0; i < items.length; i++) {
     data.chapters.push(parseInt(items[i], 10));
   }
 
-  const url = `/story/${window.document.getElementById('sortable_chapters')
-    .getAttribute('data-story')}/sort/`;
+  const storyId = window.document.getElementById('sortable_chapters')
+    .getAttribute('data-story');
+  const url = `/story/${storyId}/sort/`;
   postJSON(url, data)
     .then((response) => response.json())
-    .then((response) => {
-      core.handleResponse(response, url);
+    .then((result) => {
+      if (!result.success) {
+        notifyError(data.error);
+      } else {
+        notify('Главы отсортированы');
+      }
     })
-    .catch(core.handleError);
+    .catch((e) => notifyError(e.toString()));
 };
 
 import('jquery').then((module) => {

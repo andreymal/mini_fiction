cf20e5c37e3199bc6979e30ae985697b05fe02bb
Andriy Kushnir (Orhideous) <me@orhideous.name>
2021-05-20 04:30:16 +0300

    Enable new chunks to build

--- mini_fiction/frontend/webpack.config.babel.js
+++ mini_fiction/frontend/webpack.config.babel.js
@@ -55,6 +55,10 @@ module.exports = {
   entry: {
     story: ['./story.js', './story.css'],
     index: ['./index.js', './index.css'],
+    editor: ['./editor.js'],
+    chapter_sorting: ['./chapter_sorting.js'],
+    bootstrap: ['./bootstrap.js'],
+    slides: ['./slides.js'],
   },
 
   output: {

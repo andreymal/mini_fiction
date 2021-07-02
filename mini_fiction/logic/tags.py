d29a292ec4e774f3c6d3c37630cfd6107fcb9a11
Andriy Kushnir (Orhideous) <me@orhideous.name>
2021-07-02 17:08:41 +0300

    Preliminary support for spoiler tags

--- mini_fiction/logic/tags.py
+++ mini_fiction/logic/tags.py
@@ -1,6 +1,6 @@
 from dataclasses import dataclass
 from itertools import chain, groupby
-from typing import List
+from typing import List, Tuple
 
 from mini_fiction.models import Story, StoryTag, Tag, TagCategory
 
@@ -10,33 +10,52 @@ class PreparedTags:
     primary: List[Tag]
     secondary: List[Tag]
     extreme: List[Tag]
+    spoiler: List[Tag]
 
 
 def _group_key(tag: Tag) -> TagCategory:
     return tag.category
 
 
-def _get_prepared_tags(story_tags: List[StoryTag]) -> PreparedTags:
-    sorted_tags: List[Tag] = sorted((st.tag for st in story_tags), key=_group_key)
+def _extract_spoilers(tags: List[Tag]) -> Tuple[List[Tag], List[Tag]]:
+    groups = [
+        list(tag_group) for _, tag_group in groupby(tags, key=lambda t: t.is_spoiler)
+    ]
+    if len(groups) == 1:
+        return groups[0], []
+    return groups[0], groups[1]
 
-    if len(sorted_tags) <= 5:
-        primary = sorted_tags
-        secondary = []
-    else:
-        grouped = [
-            list(tag_group) for _, tag_group in groupby(sorted_tags, key=_group_key)
-        ]
-        primary = list(chain.from_iterable(group[:1] for group in grouped[:5]))
-        secondary = list(
+
+def _group_tags(tags: List[Tag]) -> Tuple[List[Tag], List[Tag]]:
+    grouped = [list(tag_group) for _, tag_group in groupby(tags, key=_group_key)]
+    return (
+        list(chain.from_iterable(group[:1] for group in grouped[:5])),
+        list(
             chain(
                 chain.from_iterable(group[1:] for group in grouped[:5]),
                 chain.from_iterable(grouped[5:]),
             )
-        )
+        ),
+    )
 
+
+def _get_prepared_tags(story_tags: List[StoryTag]) -> PreparedTags:
+    sorted_tags: List[Tag] = sorted((st.tag for st in story_tags), key=_group_key)
+    main_tags, spoiler = _extract_spoilers(sorted_tags)
     extreme = [t for t in sorted_tags if t.is_extreme_tag]
 
-    return PreparedTags(primary=primary, secondary=secondary, extreme=extreme)
+    if len(main_tags) <= 5:
+        secondary: List[Tag]
+        primary, secondary = main_tags, []
+    else:
+        primary, secondary = _group_tags(main_tags)
+
+    return PreparedTags(
+        primary=primary,
+        secondary=secondary,
+        extreme=extreme,
+        spoiler=spoiler,
+    )
 
 
 def get_prepared_tags(story: Story) -> PreparedTags:

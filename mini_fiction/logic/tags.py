11081ccf461888a1dc9a005d9e9f8271c0c3fea6
Andriy Kushnir (Orhideous) <me@orhideous.name>
2021-07-02 19:05:56 +0300

    Simplify tags grouping

--- mini_fiction/logic/tags.py
+++ mini_fiction/logic/tags.py
@@ -1,5 +1,7 @@
+from collections import defaultdict
 from dataclasses import dataclass
-from itertools import chain, groupby
+from itertools import chain, islice
+from operator import attrgetter
 from typing import List, Tuple
 
 from mini_fiction.models import Story, StoryTag, Tag, TagCategory
@@ -17,38 +19,38 @@ def _group_key(tag: Tag) -> TagCategory:
     return tag.category
 
 
+def _group_spoiler(tag: Tag) -> bool:
+    return tag.is_spoiler
+
+
 def _extract_spoilers(tags: List[Tag]) -> Tuple[List[Tag], List[Tag]]:
-    groups = [
-        list(tag_group) for _, tag_group in groupby(tags, key=lambda t: t.is_spoiler)
-    ]
-    if len(groups) == 1:
-        return groups[0], []
-    return groups[0], groups[1]
+    result = defaultdict(list)
+    for tag in tags:
+        result[tag.is_spoiler].append(tag)
+    return result.get(False, []), result.get(True, [])
 
 
 def _group_tags(tags: List[Tag]) -> Tuple[List[Tag], List[Tag]]:
-    grouped = [list(tag_group) for _, tag_group in groupby(tags, key=_group_key)]
-    return (
-        list(chain.from_iterable(group[:1] for group in grouped[:5])),
-        list(
-            chain(
-                chain.from_iterable(group[1:] for group in grouped[:5]),
-                chain.from_iterable(grouped[5:]),
-            )
-        ),
-    )
+    grouped = defaultdict(list)
+    for tag in tags:
+        grouped[tag.category].append(tag)
+
+    primary: List[Tag] = []
+    for tag_group in islice(grouped.values(), 0, 5):
+        primary.append(tag_group.pop(0))
+
+    secondary = list(chain.from_iterable(grouped.values()))
+
+    return primary, secondary
 
 
 def _get_prepared_tags(story_tags: List[StoryTag]) -> PreparedTags:
-    sorted_tags: List[Tag] = sorted((st.tag for st in story_tags), key=_group_key)
+    sorted_tags: List[Tag] = sorted(
+        (st.tag for st in story_tags), key=attrgetter("category", "iname")
+    )
     main_tags, spoiler = _extract_spoilers(sorted_tags)
     extreme = [t for t in sorted_tags if t.is_extreme_tag]
-
-    if len(main_tags) <= 5:
-        secondary: List[Tag]
-        primary, secondary = main_tags, []
-    else:
-        primary, secondary = _group_tags(main_tags)
+    primary, secondary = _group_tags(main_tags)
 
     return PreparedTags(
         primary=primary,

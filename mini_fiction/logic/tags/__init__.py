557e01fbd718d6337e722a708276c4178adc50e2
andreymal <andriyano-31@mail.ru>
2023-09-10 21:56:52 +0300

    Use explicit reexport to make linters happy

--- mini_fiction/logic/tags/__init__.py
+++ mini_fiction/logic/tags/__init__.py
@@ -1,13 +1,13 @@
-from .private import normalize_tag
+from .private import normalize_tag as normalize_tag
 from .public import (
-    TAG_SORTING,
-    create,
-    get_aliases_for,
-    get_all_tags,
-    get_prepared_tags,
-    get_tags_objects,
-    get_tags_with_categories,
-    search_by_prefix,
-    update,
-    delete,
+    TAG_SORTING as TAG_SORTING,
+    create as create,
+    get_aliases_for as get_aliases_for,
+    get_all_tags as get_all_tags,
+    get_prepared_tags as get_prepared_tags,
+    get_tags_objects as get_tags_objects,
+    get_tags_with_categories as get_tags_with_categories,
+    search_by_prefix as search_by_prefix,
+    update as update,
+    delete as delete,
 )
